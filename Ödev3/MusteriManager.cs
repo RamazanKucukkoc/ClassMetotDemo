bdc67d76f9f8d3342adbb0ff029b2f3fac248684
Ramazan <Ramazan@LAPTOP-L2A4OVBE>
2022-01-24 20:53:37 +0300

    Add project files.

---
+++ Ödev3/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+   public class MusteriManager
+    {
+        public void Ekleme(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Ekleme Gerçekleşti Id :"+musteri.Id+" Adı :"+ 
+                musteri.Ad+" :Soyadı :"+ musteri.Soyad);
+        }
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Listeleme Gerçekleşti Id :" + musteri.Id + " Adı :" +
+                 musteri.Ad + " :Soyadı :" + musteri.Soyad);
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Silme Gerçekleşti Id :" + musteri.Id + " Adı :" +
+                musteri.Ad + " :Soyadı :" + musteri.Soyad);
+        }
+
+
+
+    }
+}
---
+++ Ödev3/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClassMetotDemo;
+using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Ramazan";
+            musteri1.Soyad = "Küçükkoç";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ahmet";
+            musteri2.Soyad = "Sahin";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Ali";
+            musteri3.Soyad = "Kartal";
+
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri[] musteriler = new Musteri[] {
+            musteri1,musteri2,musteri3
+            };     
+            musteriManager.Ekleme(musteri3);
+            Console.WriteLine("-------------------Ekleme Bitti----------------");    
+            foreach (var item in musteriler)
+            {
+                musteriManager.Listeleme(item);
+            }
+            Console.WriteLine("-------------------Listeleme Bitti----------------");
+            musteriManager.Silme(musteri2);
+            Console.WriteLine("-------------------Silme Bitti----------------");
+        }
+    }
+}
